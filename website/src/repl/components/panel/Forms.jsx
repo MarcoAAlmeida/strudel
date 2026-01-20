35a3e32238127bb55467a6913e77298b78113159
Felix Roos <flix91@gmail.com>
2026-01-20 23:08:31 +0100

    no bg on button group

--- website/src/repl/components/panel/Forms.jsx
+++ website/src/repl/components/panel/Forms.jsx
@@ -9,7 +9,7 @@ export function ButtonGroup({ value, onChange, items }) {
           id={key}
           onClick={() => onChange(key)}
           className={cx(
-            'px-2 border-b-2 h-8 whitespace-nowrap bg-background',
+            'px-2 border-b-2 h-8 whitespace-nowrap',
             // i === 0 && 'rounded-l-md',
             // i === arr.length - 1 && 'rounded-r-md',
             // value === key ? 'bg-background' : 'bg-lineHighlight',
