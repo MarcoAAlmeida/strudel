41045c12a644ceff21edd419fa75438a7218a455
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2026-01-21 20:56:14 -0300

    attempted to fix github build issue

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -21,4 +21,25 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const articles = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    description: z.string().optional(),
+    // Transform string to Date object
+    date: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+export const collections = { blog, articles };
