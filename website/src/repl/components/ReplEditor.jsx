3364b9f05fc8ed98188e36eb6a94302442cc9905
Felix Roos <flix91@gmail.com>
2026-01-22 10:12:16 +0100

    keep it simple stupid

--- website/src/repl/components/ReplEditor.jsx
+++ website/src/repl/components/ReplEditor.jsx
@@ -1,8 +1,7 @@
-import Loader from '@src/repl/components/Loader';
-import { HorizontalPanel, VerticalPanel, PanelToggle } from '@src/repl/components/panel/Panel';
 import { Code } from '@src/repl/components/Code';
+import Loader from '@src/repl/components/Loader';
+import { HorizontalPanel, MainPanel, VerticalPanel } from '@src/repl/components/panel/Panel';
 import UserFacingErrorMessage from '@src/repl/components/UserFacingErrorMessage';
-import { Footer, Header } from './Header';
 import { useSettings } from '@src/settings.mjs';
 
 // type Props = {
@@ -13,23 +12,23 @@ export default function ReplEditor(Props) {
   const { context, ...editorProps } = Props;
   const { containerRef, editorRef, error, init, pending } = context;
   const settings = useSettings();
-  const { panelPosition, isZen, isButtonRowHidden } = settings;
+  const { panelPosition, isZen } = settings;
   const isEmbedded = typeof window !== 'undefined' && window.location !== window.parent.location;
 
   return (
     <div className="h-full flex flex-col relative" {...editorProps}>
       <Loader active={pending} />
-      <div className="grow flex justify-stretch w-full relative overflow-hidden">
-        <div className="flex flex-col grow overflow-hidden">
-          <Header context={context} isEmbedded={isEmbedded} />
+      <div className="flex flex-col grow overflow-hidden">
+        {/* <MainPanel context={context} isEmbedded={isEmbedded} className="hidden sm:block" /> */}
+        <MainPanel context={context} isEmbedded={isEmbedded} />
+        <div className="flex overflow-hidden">
           <Code containerRef={containerRef} editorRef={editorRef} init={init} />
-          {/* <Footer context={context} isEmbedded={isEmbedded} /> */}
+          {!isZen && panelPosition === 'right' && <VerticalPanel context={context} />}
         </div>
-        {!isZen && panelPosition === 'right' && <VerticalPanel context={context} />}
       </div>
-      {!isButtonRowHidden && <Footer context={context} isEmbedded={isEmbedded} />}
       <UserFacingErrorMessage error={error} />
       {!isZen && panelPosition === 'bottom' && <HorizontalPanel context={context} />}
+      {/* <MainPanel context={context} isEmbedded={isEmbedded} className="block sm:hidden" /> */}
     </div>
   );
 }
